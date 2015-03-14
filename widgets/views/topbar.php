ec18ea3afbb80772ab00467b59453955e84c2b49
Paul Purcel <bigpaulie25ro@yahoo.com>
2015-03-14 21:57:34 +0200

    Fix typo

--- widgets/views/topbar.php
+++ widgets/views/topbar.php
@@ -17,37 +17,7 @@
         <?php if (isset($items['right'])): ?>
             <ul class="right">
                 <?php foreach ($items['right'] as $r): ?>
-                    <?php if (isset($r['class']) && $r['class'] == 'divider'): ?>
-                        <li class="divider"></li>
-                    <?php elseif (isset($r['class']) && $r['class'] == 'active'): ?>
-                        <li class="active">
-                            <a href="<?php echo $r['link']; ?>">
-                                <?php echo $r['lable']; ?>
-                            </a>
-                        </li>
-                    <?php elseif (is_array($r)): ?>
-                        <?php foreach ($r as $ra): ?>
-                            <li class="has-dropdown">
-                                <?php die(print_r($r));?>
-                                <a href="#"><?php echo $ra['lablesub']; ?></a>
-                                <ul class="dropdown">
-                                    <?php foreach ($ra['items'] as $ri): ?>
-                                        <li>
-                                            <a href="<?php echo $ri['link']; ?>">
-                                                <?php echo $ri['lable']; ?>
-                                            </a>
-                                        </li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            </li>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <li>
-                            <a href="<?php echo $r['link']; ?>">
-                                <?php echo $r['lable']; ?>
-                            </a>
-                        </li>
-                    <?php endif; ?>
+                    <pre><?php print_r($r); ?></pre>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
