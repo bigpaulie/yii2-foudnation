bf1836cca2f4139951b122a200f947104c559e73
Paul Purcel <bigpaulie25ro@yahoo.com>
2015-03-14 21:01:23 +0200

    Topbar Widget

--- widgets/Topbar.php
+++ widgets/Topbar.php
@@ -4,18 +4,23 @@ namespace bigpaulie\foundation\widgets;
 
 use bigpaulie\foundation\Widget;
 
+/**
+ * Topbar Widget 
+ * @todo add has-form, fixed and contain-to-grid options
+ * @author Paul Purcel <bigpaulie25ro@yahoo.com>
+ */
 class Topbar extends Widget {
 
     public $items = [];
-    public $title;
-    public $titleLink;
+    public $lable;
+    public $link;
 
     public function run() {
         $this->registerOptions();
         return $this->render('topbar', [
                     'items' => $this->items,
-                    'title' => $this->title,
-                    'titleLink' => $this->titleLink
+                    'lable' => $this->lable,
+                    'link' => $this->link
         ]);
     }
 
--- widgets/views/topbar.php
+++ widgets/views/topbar.php
@@ -2,10 +2,10 @@
 
     <ul class="title-area">
         <li class="name">
-            <?php if (isset($titleLink) && !empty($titleLink)): ?>
-            <h1><a href="<?php echo $titleLink;?>"><?php echo $title;?></a></h1>
+            <?php if (isset($link) && !empty($link)): ?>
+                <h1><a href="<?php echo $link; ?>"><?php echo $lable; ?></a></h1>
             <?php else: ?>
-                <h1><?php echo $title;?></h1>
+                <h1><?php echo $lable; ?></h1>
             <?php endif; ?>
         </li>
         <li class="toggle-topbar menu-icon">
@@ -14,17 +14,84 @@
     </ul>
 
     <section class="top-bar-section">
-        <ul class="right">
-            <li class="active">
-                <a href="#">Home</a>
-            </li>
-        </ul>
-
-        <ul class="left">
-            <li class="active">
-                <a href="#">Menu Item</a>
-            </li>
-        </ul>
+        <?php if (isset($items['right'])): ?>
+            <ul class="right">
+                <?php foreach ($items['right'] as $r): ?>
+                    <?php if ($r['class'] == 'active'): ?>
+                        <li class="active">
+                            <a href="<?php echo $r['link']; ?>"><?php echo $r['label']; ?></a>
+                        </li>
+                    <?php elseif (is_array($r)): ?>
+                        <li class="has-dropdown">
+                            <a href="#"><?php echo $r['lable']; ?></a>
+                            <ul class="dropdown">
+                                <?php foreach ($r['items'] as $ri): ?>
+                                    <?php if ($ri['class'] == 'active'): ?>
+                                        <li class="active">
+                                            <a href="<?php echo $ri['link']; ?>">
+                                                <?php echo $ri['label']; ?>
+                                            </a>
+                                        </li>
+                                    <?php else: ?>
+                                        <li>
+                                            <a href="<?php echo $ri['link']; ?>">
+                                                <?php echo $ri['label']; ?>
+                                            </a>
+                                        </li>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    <?php else: ?>
+                        <li>
+                            <a href="<?php echo $r['link']; ?>">
+                                <?php echo $r['label']; ?>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        
+        <?php if (isset($items['left'])): ?>
+            <ul class="left">
+                <?php foreach ($items['left'] as $l): ?>
+                    <?php if ($l['class'] == 'active'): ?>
+                        <li class="active">
+                            <a href="<?php echo $l['link']; ?>"><?php echo $l['label']; ?></a>
+                        </li>
+                    <?php elseif (is_array($l)): ?>
+                        <li class="has-dropdown">
+                            <a href="#"><?php echo $l['lable']; ?></a>
+                            <ul class="dropdown">
+                                <?php foreach ($l['items'] as $li): ?>
+                                    <?php if ($li['class'] == 'active'): ?>
+                                        <li class="active">
+                                            <a href="<?php echo $li['link']; ?>">
+                                                <?php echo $li['label']; ?>
+                                            </a>
+                                        </li>
+                                    <?php else: ?>
+                                        <li>
+                                            <a href="<?php echo $li['link']; ?>">
+                                                <?php echo $li['label']; ?>
+                                            </a>
+                                        </li>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    <?php else: ?>
+                        <li>
+                            <a href="<?php echo $l['link']; ?>">
+                                <?php echo $l['label']; ?>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        
     </section>
 
 </nav>
